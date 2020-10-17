0e1dca455c66bcf40ef3674684a09f989afd922d
8pg <paulo.godoy@systempro.com.br>
2020-10-17 09:30:01 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+
+/**
+ *
+ * @author P. Godoy
+ */
+public interface DepartmentDao {
+
+    public void insert(Department obj);
+
+    public void update(Department obj);
+
+    public void deleteByID(Integer id);
+
+    public void findByID(Integer id);
+
+    public void findAll(List<Department> list);
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Seller;
+
+/**
+ *
+ * @author P. Godoy
+ */
+public interface SellerDao {
+
+    public void insert(Seller obj);
+
+    public void update(Seller obj);
+
+    public void deleteByID(Integer id);
+
+    public void findByID(Integer id);
+
+    public void findAll(List<Seller> list);
+}
