ec7c4a3ff8ce00c6c18fd7948849f0b0e35198ef
8pg <paulo.godoy@systempro.com.br>
2020-10-17 09:14:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.Date;
+import model.entities.Department;
+import model.entities.Seller;
+
 /**
  *
  * @author 8pg
@@ -8,6 +12,11 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Department dep = new Department(5, "Camping");
+        Seller sel = new Seller(8, "Jose De La Cruz", "ze@gmail.com", new Date(), 1586.00, dep);
+
+        System.out.println(dep);
+        System.out.println(sel);
     }
 
 }
